273a80dce78a57bd03266ae26c360b8b82a7a339
ehitchman-dev <142246557+ehitchman-dev@users.noreply.github.com>
2023-11-30 18:58:46 -0500

    admin

--- classes/OpenWeatherMap.py
+++ classes/OpenWeatherMap.py
@@ -9,7 +9,7 @@ from classes.LoggingClass import LoggingManager
 
 runtime_logger_level = 'DEBUG'
 
-class OpenWeatherMapForecasts():
+class WeatherForecastRetriever():
 
     def __init__(self):
 
@@ -192,129 +192,8 @@ class OpenWeatherMapForecasts():
 
         return forecast_df
 
-    # #Use pandas to group by date and name and location and then take the max of 
-    # # "forecast_weather_category_values" by DAY
-    # def compareWeatherResults(
-    #         self,
-    #         list_of_all_forecast_dfs, 
-    #         is_testing_run=True
-    #         ):
-        
-    #     import pandas as pd
-
-    #     #NOTE/QUESTION How best to loop through a list of dfs and merge on each 
-    #     for i, forecast_df in enumerate(list_of_all_forecast_dfs):
-
-    #         ################
-    #         if is_testing_run == True:
-    #             i=0 #NOTE/TESTING THIS IS FOR TESTING ONLY
-    #             self.logger.info('compareWeatherResults() is running, test run:', is_testing_run)
-    #         else:
-    #             self.logger.info('compareWeatherResults() is running, test run:', is_testing_run)
-    #         ################
-
-    #         #Slices he list of dataframes to get just the nth dataframe and then:
-    #         # LOOP repeat until done list of dataframes captured from openweathermap
-    #         #  based on the yaml config files' lon/lat coordinates  
-    #         # - if 1st run, creates a dataframe from the 1st in the list of dfs the function is called with 
-    #         # - if nth run, merges the nth dataframe from the list with the previous iterations version 
-    #         #    of forecastsandweathercat_df on rain_category, forecast_datestring columns
-    #         # - filters result to only the rain_category, forecast_datestring columns
-    #         # - the resulting dataframe contains only forecast_datestrings where there is rain for all parties
-    #         if i == 0:
-    #             forecastsandweathercat_df = pd.DataFrame()
-    #             forecastsandweathercat_df = list_of_all_forecast_dfs[i]
-    #             forecastsandweathercat_df = forecastsandweathercat_df.loc[: ,['rain_category','forecast_datestring']]
-    #         else:
-    #             forecast_df=list_of_all_forecast_dfs[i]
-    #             forecast_df = forecast_df.loc[: ,['rain_category','forecast_datestring']]
-    #             forecastsandweathercat_df = pd.merge(forecastsandweathercat_df, forecast_df,
-    #                                                 on = ['rain_category', 'forecast_datestring'])
-    #             #TODO add a step which filters the erge to 
-    #             forecastsandweathercat_df =  forecastsandweathercat_df.loc[(forecastsandweathercat_df['rain_category'] == 'rain'),:]
-
-    #     ###################
-    #     #ALTERNATIVE METHOD
-    #     #TODO concatenate all dfs from df list into a single table for use with pandas
-    #     #df = pd.concat(list_of_all_forecast_dfs)
-    #     #self.logger.info(df)
-        
-    #     #TODO Pivot data to make easy for comparison (finding all 1s)
-    #     #3. Compare values for each day to see if there is overlap
-    #     ###################
-
-    #     #TODO/FEATURE output should look like a  list of days/times/users that can meet
-    #     #   -time    -[users] -reason
-    #     #   2023-06-09 00:00    [eric, crube, nano] 'rain'
-    #     #   2023-06-09 04:00    [crube, oath, prag] 'cuz its saturday'
-        
-    #     self.logger.info(f' - compareWeatherResults() finished, return object is of type: {type(forecastsandweathercat_df)}"')
-    #     return forecastsandweathercat_df
-    #     #EOF
-
-    # #Function gathers a ditionary for use in bot.py.  Uses getWeatherForecast, 
-    # # transformJsonForecast, transformJsonForecast_table, compareWeatherResults 
-    # def getRaindayGamedayOptions(self):
-
-    #     ############################################################
-    #     #TODO change envfile_path_string in config.yml
-    #     # - Update the uri for the env path.  Create a template env file, etc.
-    #     # - End goal is to make script easily usable by others
-        
-
-    #     list_of_forecast_dfs = []
-    #     list_of_all_forecast_dfs = []
-    #     list_of_all_users_names = []
-    #     dict_returned_objects = {}
-
-    #     for i, user in enumerate(self.config.users_details):
-            
-    #         #get users_detail list and details
-    #         user_details = self.config.users_details[i]
-    #         user_name = user_details['name']
-    #         user_lon = user_details['lon']
-    #         user_lat = user_details['lat']
-    #         self.logger.info("Getting forecast for", user_name)
-
-    #         #NOTE does it make sense to be calling a function directly inside of a 
-    #         # function?  Is this a sign of poor design?
-    #         json_forecast = self.getWeatherForecast(
-    #             user_name = user_name,
-    #             lon = user_lon,
-    #             lat = user_lat,
-    #             write_to_directory=True,
-    #             i = i)
-            
-    #         self.logger.info(f'Finished call to getWeatherForecast for {user_name}')
-
-    #         #Run transformJsonForecast
-    #         list_of_forecast_dfs = self.transformJsonForecast(json_user_forecast = json_forecast)
-    #         #self.logger.info("list_of_forecast_dfs is of type:", type(list_of_forecast_dfs))
-    #         self.logger.info(f'Finished call to transformJsonForecast for {user_name}')
-
-    #         #append df to new all forecasts list item and users name list item
-    #         list_of_all_forecast_dfs.append(list_of_forecast_dfs)
-    #         list_of_all_users_names.append(user_name)
-
-    #     #self.logger.info logs
-    #     self.logger.info("returned list_of_all_forecast_dfs of type", type(list_of_all_forecast_dfs), 'and length', len(list_of_all_forecast_dfs))
-
-    #     #TODO concatenate all dfs from df list into a single table for use with pandas
-    #     raindaygameday = self.compareWeatherResults(list_of_all_forecast_dfs = list_of_all_forecast_dfs,
-    #                                         is_testing_run=False)
-    #     self.logger.info(f'Finished call to compareWeatherResults for {user_name}')
-    #     raindaygameday = raindaygameday['forecast_datestring']
-
-    #     #add objects to return dictionary
-    #     dict_returned_objects['dates_and_times_df'] = raindaygameday
-    #     dict_returned_objects['users_names_list'] = list_of_all_users_names
-
-    #     #return dictionary
-    #     self.logger.info('EoF: return object is of type:',type(dict_returned_objects))
-    #     return dict_returned_objects
-
 if __name__ =="__main__":
-    owm = OpenWeatherMapForecasts()
+    owm = WeatherForecastRetriever()
     config = ConfigManager(yaml_filepath='config', yaml_filename='config.yaml')
     os.path.join(config.log_responses_directory, config.response_file_name+'_.json')
     print(f"Testing retrieval of ConfigClass variable. config.env_filename:{owm.config.env_filename}")
