23af03d371ec0c97e3fb0d43f7f773d97137c225
ehitchman-dev <142246557+ehitchman-dev@users.noreply.github.com>
2023-12-01 13:54:48 -0500

    removed cahche/retry session from requests_cache

--- classes/OpenMeteoWeatherClass.py
+++ classes/OpenMeteoWeatherClass.py
@@ -1,8 +1,6 @@
 import openmeteo_requests
 
-import requests_cache
 import pandas as pd
-import numpy as np
 from datetime import datetime, timedelta
 from retry_requests import retry
 
@@ -12,15 +10,19 @@ from classes.GCS import GCSManager
 class WeatherHistoryRetriever:
     def __init__(self):
         # Setup the Open-Meteo API client with cache and retry on error
-        cache_session = requests_cache.CachedSession('.cache', expire_after=-1)
-        retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
-        self.openmeteo = openmeteo_requests.Client(session=retry_session)
+        #cache_session = requests_cache.CachedSession('.cache', expire_after=-1)
+        #retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+        #self.openmeteo = openmeteo_requests.Client(session=retry_session)
+        self.openmeteo = openmeteo_requests.Client()
 
         # Setup Config manager and GCS client
         self.config = ConfigManager(yaml_filepath='config', yaml_filename='config.yaml')
         self.gcs_manager = GCSManager()
 
+        self.date_today = datetime.now().strftime('%Y-%m-%d')
+
     def fetch_and_process(self, latitude, longitude, start_date, end_date, name):
+        # Fetch the response
         url = "https://archive-api.open-meteo.com/v1/archive"
         params = {
             "latitude": latitude,
@@ -62,21 +64,12 @@ class WeatherHistoryRetriever:
 
         return df
 
-    def send_df_to_bq(self, df_union):
-        print("Do something...")
-        self.gcs_manager.write_df_to_gcs(
-            df=df_union, 
-            bucket_name = self.config.bucket_name, 
-            gcs_bucket_filepath = self.config.wthr_historic_unioned_csvpath
-            )
-        message = "Looks like the write_df_to_gcs() has completed successfully"
-        return message
-    
 # Example usage
 if __name__ == "__main__":
     config = ConfigManager(yaml_filepath='config', yaml_filename='config.yaml')
+    gcs_manager = GCSManager()
     wthr_history_retriever = WeatherHistoryRetriever()
-
+    date_today = datetime.now().strftime('%Y-%m-%d')
     users_details = config.users_details
 
     dfs = []
@@ -94,5 +87,4 @@ if __name__ == "__main__":
         dfs.append(df)
     
     df_unioned = pd.concat(dfs, ignore_index=True)
-
-    wthr_history_retriever.send_df_to_bq(df_union=df_unioned)
+    print(df_unioned.head(5))
