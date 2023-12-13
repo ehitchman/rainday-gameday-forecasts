1dbc92a693a6a432e7867036d976d3a542f038a5
ehitchman-dev <142246557+ehitchman-dev@users.noreply.github.com>
2023-12-13 12:43:42 -0500

    potential fix to problems where datetime is incorrect format

--- utils/__dateUtilities.py
+++ utils/__dateUtilities.py
@@ -2,8 +2,6 @@
 def convert_datetime_format(dt_str):
     from datetime import datetime
     try:
-        # Parse the datetime string and format it to include the seconds
         return datetime.strptime(dt_str, '%Y-%m-%d %H:%M').strftime('%Y-%m-%d %H:%M:%S')
     except ValueError:
-        # In case of a formatting error, return the original string
         return dt_str
