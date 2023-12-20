a3aea49a57d37a795d3e9ff3817f918cc7d6046f
ehitchman-dev <142246557+ehitchman-dev@users.noreply.github.com>
2023-12-20 10:16:55 -0500

    one-time-use for doiwnloading backup

--- utils/__download_all_gcs_files_from_folder.py
+++ utils/__download_all_gcs_files_from_folder.py
@@ -7,8 +7,9 @@ config = ConfigManager(yaml_filename='config.yaml', yaml_filepath='config')
 gcs_client = storage.Client()
 gcs_manager = GCSManager()
 
+print(config.wthr_historic_folderpath)
 gcs_manager.download_all_files_in_gcs_folder(
     bucket_name = config.bucket_name, 
-    folder_path = config.wthr_forecast_folderpath, 
+    folder_path = config.wthr_historic_folderpath, 
     destination_folder = config.primary_gcs_download_folder
 )
